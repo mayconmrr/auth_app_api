96646a7a39ca37439234aa0af9baa5cbabde706f
mayconmrr <maycon.mrr@gmail.com>
2020-08-31 16:34:27 -0700

    Add api and v1 namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  resources :users, only: :create
 
-  post 'authenticate', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  namespace :api do
+    namespace :v1 do
+      resources :users, only: :create
+
+      post 'authenticate', to: 'sessions#create'
+      delete 'logout', to: 'sessions#destroy'
+    end
+  end
 end
--- spec/requests/session_controller_spec.rb
+++ spec/requests/session_controller_spec.rb
@@ -1,15 +1,15 @@
-describe '#create' do
+RSpec.describe 'Session', type: :request do
   context 'login' do
     let(:user) { User.create(username: 'maycon_rosa', password: 'new_pass') }
 
     it 'should return 200 when login is successful' do
-      post '/authenticate', params: { username: user.username, password: 'new_pass' }
+      post '/api/v1/authenticate', params: { username: user.username, password: 'new_pass' }
 
       expect(response.status).to eq(200)
     end
 
     it 'should return 401 when login is not successful' do
-      post '/authenticate', params: { username: user.username, password: 'wrong_pass' }
+      post '/api/v1/authenticate', params: { username: user.username, password: 'wrong_pass' }
 
       expect(response.status).to eq(401)
     end
